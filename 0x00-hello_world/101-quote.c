6d1c7723b35d0c76fabc9566375d97abbb6aa5db
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-08 21:52:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
- *
+ * filedes - description
+ * nbyte - description
+ * nbuf - description
  * Return: Always 1 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar,
-		2015-10-19\n", 59);
-return (1);
+write(2, "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }

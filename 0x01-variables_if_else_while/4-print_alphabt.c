e62a58173a48e5875a8591bbf063ca800685bf67
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-09 17:04:48 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,12 @@
  * Return: Always 0.
  */
 int main(void)
-{	char letter;
+{char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
 	putchar('\n');
 	return (0);
 }

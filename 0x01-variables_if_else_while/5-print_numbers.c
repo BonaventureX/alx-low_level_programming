a2f47afa1948d251d3285bada62b83ea54ee81b3
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-09 17:20:03 -0700

    mu first 2in1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase, save q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{int num;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}

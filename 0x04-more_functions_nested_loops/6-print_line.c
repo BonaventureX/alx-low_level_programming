a1ed008c9d9d196e7ea12311886b57c2d9eaf4e7
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-15 14:25:49 -0700

    combined

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draw a str line using _
+ * @n: number of chararcters
+ *
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+
+}

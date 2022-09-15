b99bdf0e0e2a2c436a5e1e135e100757b0ad6f9d
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-15 12:06:43 -0700

    morefunctions1

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 98 fib numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int count;
+
+}
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase like in ctyp library
+ * @c: The cahracter to be checked
+ *
+ * Retun: 1 if charater is uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		retun (1);
+
+	else
+		return(0);
+}

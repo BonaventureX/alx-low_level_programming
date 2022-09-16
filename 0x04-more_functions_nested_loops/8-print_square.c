de7ab053108e9b99a2516100558ec42c881b6629
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-15 17:56:57 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,13 @@ void print_square(int size)
 {
 	int height, width;
 
-	if (size > 0)
-	{
-		for (height = 0; height < size; height++)
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 0; height < size; height++)
 		{
 			for (width = 0; width < size; width++)
 				_putchar('#');
-
-			if (height == size - 1)
-				continue;
 			_putchar('\n');
 		}
 	}

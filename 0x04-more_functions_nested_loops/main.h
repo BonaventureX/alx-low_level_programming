d7eaaec10456229fd68a19239efc864ceb67c130
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-16 07:22:31 -0700

    header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,8 +11,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square;
-void print_triangle;
-void print_number;
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif

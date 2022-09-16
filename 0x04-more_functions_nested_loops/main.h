36f3e5de9f29b40231f5da7ef49b7c16f3ebaca4
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-15 18:18:50 -0700

    maincomp

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,6 +12,10 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+void print_square;
+void print_triangle;
+void print_number;
 
 
 

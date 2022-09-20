2d0ef79b7d380e93f768e2da42b3aebae4a955da
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-19 19:30:10 -0700

    test4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse and \n
+ * @s: string to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s++)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}

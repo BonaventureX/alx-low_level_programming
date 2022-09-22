226556a6a184fde46301dd9b48030a12c9284712
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-22 10:35:43 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,15 +4,15 @@
  * string_toupper - changes lowercase letters to uppercase in a string
  * @str: string to be changed
  *
- * Return: pointer to the changed string
- *
+ * Return: @str
  */
 
-char *string_toupper(char *)
+
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;

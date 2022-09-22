2599467935181b80b65bc1823d764851c2531178
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-22 14:33:24 -0700

    continuelater

---
+++ 0x06-pointers_arrays_strings/leet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string to 1337
+ * @str: string to be encoded
+ *
+ * Return: poinnter to the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0, j;
+	char leet [] = {'a''A''e''E''o''O''t''T''l''L'}

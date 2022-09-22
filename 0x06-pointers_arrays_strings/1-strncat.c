404abaad99aa5120b897021357161d3301222d82
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-22 08:39:27 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,11 @@
  * _strncat - concatenates two strings similar to _strcat
  * @src: souruce string
  * @dest: the resulting string
- *
+ * @n: most bytes from source
  * Return: point to dest
  */
 
-char *_strncat()
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, dest_len = 0;
 

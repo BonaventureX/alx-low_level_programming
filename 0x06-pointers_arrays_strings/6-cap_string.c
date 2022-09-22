79a982e3ed852109ae9c5c78df4b678233aa8573
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-22 11:24:26 -0700

    longnote

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words
+ * @str: string to be changed
+ *
+ * Return: @str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+
+		if (i == 0)
+		{
+		str[i] -= 32;
+		}
+
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||)
+		{
+			str[i] -= 32;
+		}
+
+		}
+
+		i++;
+
+		}
+
+		return (str);
+}

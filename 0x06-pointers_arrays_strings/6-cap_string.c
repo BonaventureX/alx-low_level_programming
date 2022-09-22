89ed6fa7ede144850b0d0f7fd8dfddad62e48fa4
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-09-22 11:30:03 -0700

    longnote

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,12 +16,12 @@ char *cap_string(char *str)
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 
-		if (i == 0)
+		else if (i == 0)
 		{
 		str[i] -= 32;
 		}
 
-		if (str[i - 1] == ' ' ||
+		else if (str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||

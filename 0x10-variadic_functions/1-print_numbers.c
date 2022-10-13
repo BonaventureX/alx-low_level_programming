c3c37d9fddbab9aeac5178ee143372b4891137db
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-10-13 23:15:35 +0000

    wrong file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,34 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
+
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
+ * print_numbers - Prints numbers, followed by a new line.
  *
- * Return: sum
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  *
  */
 
-int sum_them_all(const unsigned int n, ...)
+
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
 	unsigned int i;
-	unsigned int sum = 0;
 
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(num, int);
+	{
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
 
 	va_end(num);
-
-	return (sum);
 }

014c0018111764f3b84c29e101d81c835c03ad6a
BonaventureX <nwangwu.bonaventure@yahoo.com>
2022-10-27 20:50:12 +0000

    0.0

---
+++ 0x014-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to uint
+ *
+ * @b: pointer to a string containing the binary number
+ *
+ * Return: unsigned int with decimal equivalent of binary num
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
+}
